82fac4d2d10d98a33c4c0dda91dfc44ec77776b5
hyoom <ghkddbstj@naver.com>
2024-09-06 17:24:45 +0900

    초기화

---
+++ src/main/java/ktb/chat/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package ktb.chat.config;
+
+import ktb.chat.handler.WebSocketChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketChatHandler webSocketChatHandler;
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketChatHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
