ebe331b38663d317af8ca4b719d193b74b13c4c9
cjm0611 <cjm-0611@naver.com>
2024-09-06 18:05:17 +0900

    feat: DTO에 프로필 이미지 추가

--- src/main/java/ktb/chat/model/ChatMessageDto.java
+++ src/main/java/ktb/chat/model/ChatMessageDto.java
@@ -16,6 +16,7 @@ public class ChatMessageDto {
 
     private MessageType messageType; // 메시지 타입
     private Long chatRoomId; // 방 번호
-    private Long senderId; // 채팅을 보낸 사람
+    private String nickname; // 채팅을 보낸 사람
     private String message; // 메시지
+    private String profileImage; // 프로필 이미지
 }
